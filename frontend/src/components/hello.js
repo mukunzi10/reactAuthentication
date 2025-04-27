8bceb41e9e64c876281bdd8585d6dffb3274344a
mukunzi10 <mukunzigustavus@gmail.com>
2025-04-27 16:38:56 +0200

    react authentication for beginner

---
+++ frontend/src/components/hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const hello = () => {
+  return (
+    <div>
+        <div className="flex flex-col items-center justify-center h-screen bg-blue-500"></div>
+        <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+        hello</div>
+  )
+}
+
+export default hello
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/App.js',
+    './src/components/hello.js',],
+
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
